33773745e8c75a6087028bb9116a1ddb21093095
Primulaa <94114990+Primulaa@users.noreply.github.com>
2022-06-05 14:07:41 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,36 @@
+const staticCacheName = 'site-static-v1';
+const assets = [
+  '/02/',
+  '/02/index.html',
+  '/assets/js/ui.js',
+  '/assets/css/main.css',
+  ];
+  
+// событие install
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log('caching shell assets');
+      cache.addAll(assets);
+    })
+  );
+});
+// событие activate
+self.addEventListener('activate', evt => {
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== staticCacheName)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+// событие fetch
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request);
+    })
+  );
+});
